29492213eed78af426c21edea5df42f7c09e9f8e
camargo <camargo@CAMARGO>
2024-08-26 22:26:19 -0300

    Adicionar arquivos de projeto.

---
+++ Decrapify Windows 10/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Decrapify_Windows_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Title = new System.Windows.Forms.Label();
+            this.btnExecuteAll = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Title.Font = new System.Drawing.Font("Poppins", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Title.Location = new System.Drawing.Point(12, 20);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(162, 48);
+            this.Title.TabIndex = 13;
+            this.Title.Text = "Decrapify ";
+            this.Title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Title.Click += new System.EventHandler(this.Title_Click);
+            // 
+            // btnExecuteAll
+            // 
+            this.btnExecuteAll.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnExecuteAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExecuteAll.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnExecuteAll.Location = new System.Drawing.Point(20, 86);
+            this.btnExecuteAll.Name = "btnExecuteAll";
+            this.btnExecuteAll.Size = new System.Drawing.Size(99, 41);
+            this.btnExecuteAll.TabIndex = 14;
+            this.btnExecuteAll.Text = "Decrapify!";
+            this.btnExecuteAll.UseVisualStyleBackColor = false;
+            this.btnExecuteAll.Click += new System.EventHandler(this.btnExecuteAll_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(601, 328);
+            this.Controls.Add(this.btnExecuteAll);
+            this.Controls.Add(this.Title);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Camargo Decrapify - v1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Button btnExecuteAll;
+    }
+}
+
---
+++ Decrapify Windows 10/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Decrapify_Windows_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ListEmbeddedResources();
+        }
+
+        private void btnExecuteAll_Click(object sender, EventArgs e)
+        {
+            RunEmbeddedPowerShellScript("debloat.ps1");
+        }
+
+        private void RunEmbeddedPowerShellScript(string scriptName)
+        {
+            string namespaceName = Assembly.GetExecutingAssembly().GetName().Name;
+            string resourceName = $"{namespaceName}.{scriptName}";
+            string script = GetEmbeddedResource(resourceName);
+            if (script != null)
+            {
+                RunPowerShellScript(script);
+            }
+            else
+            {
+                MessageBox.Show("Script não encontrado: " + resourceName, "Erro");
+            }
+        }
+
+        private string GetEmbeddedResource(string resourceName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                if (stream == null)
+                {
+                    return null;
+                }
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+
+        private void RunPowerShellScript(string scriptContent)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = "powershell.exe";
+            psi.Arguments = $"-Command \"{scriptContent}\"";
+            psi.RedirectStandardOutput = true;
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+
+            Process process = new Process();
+            process.StartInfo = psi;
+            process.Start();
+            process.WaitForExit();
+
+            // Opcional: Capturar e exibir a saída do script
+            string output = process.StandardOutput.ReadToEnd();
+            MessageBox.Show(output, "Resultado do Script");
+        }
+
+        private void ListEmbeddedResources()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            string[] resourceNames = assembly.GetManifestResourceNames();
+            foreach (string resourceName in resourceNames)
+            {
+                Console.WriteLine(resourceName);
+            }
+        }
+
+    }
+}
